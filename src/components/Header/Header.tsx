997065fa9c43b3ea7edeb1df6b384eb76901553b
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2025-01-29 00:01:52 -0300

    adiciona componente Header com logo e mensagem de boas-vindas

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import logo_side_pink from '../../assets/logo_side_full_transparent_pink.svg';
+import logo_bottom_pink from '../../assets/logo_bottom_full_transparent_pink.svg';
+import styles from './Header.module.css';
+import { useWindowDimensions } from '../../hooks';
+
+export const Header = () => {
+	const { windowWidth } = useWindowDimensions();
+	return (
+		<div className={styles.container}>
+			<img
+				className={styles.logo}
+				src={windowWidth > 480 ? logo_side_pink.src : logo_bottom_pink.src}
+				alt='Meeple rosa com a escrita Espírito Lúdico e o slogan Joga e conecta'
+			/>
+			<div className={styles.welcomeContainer}>
+				<h2 className={styles.welcomeText}>Boas vindas!</h2>
+				<p className={styles.welcomeSubtext}>
+					Joga como se a diversão dependesse de você, conecta como se a magia do
+					momento dependesse de todos.
+				</p>
+			</div>
+			{windowWidth <= 880 && (
+				<button className={styles.wannaPlayBtn}>Quero jogar</button>
+			)}
+		</div>
+	);
+};
