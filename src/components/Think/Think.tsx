5cefd24b63bbbfe6db342d57f64f72faa77fc4ef
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2025-02-03 01:17:40 -0300

    refatora componente Think, substituindo elementos por componentes Title e Button, e ajusta estilos no CSS

--- src/components/Think/Think.tsx
+++ src/components/Think/Think.tsx
@@ -1,21 +1,19 @@
 import styles from './Think.module.css';
 import gamesImg from '../../assets/games.jpg';
+import { Button } from '../Button';
+import { Title } from '../Title';
 
 export const Think = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<div className={styles.titleContainer}>
-					<h3 className={styles.title}>
-						Já pensou em alugar jogos de tabuleiro?
-					</h3>
-					<div className={styles.line}></div>
-				</div>
+				<Title >Já pensou em alugar jogos de tabuleiro?</Title>
 				<p className={styles.subtext}>
 					Desperte o espírito lúdico em você e crie conexões autênticas e
-					momentos inesquecíveis com quem você ama. Tudo isso do conforto da sua casa.
+					momentos inesquecíveis com quem você ama. Tudo isso do conforto da sua
+					casa.
 				</p>
-				<button className={styles.button}>Saiba mais</button>
+				<Button>Saiba mais</Button>
 			</div>
 			<img className={styles.image} src={gamesImg.src} alt='' />
 		</div>
